1a7479a508c9c91cce1964a380144d4f3ccd867f
manelbenslimen <benslimenmanel94@gmail.com>
2020-06-15 16:51:05 +0200

    design WIP

--- renderer.js
+++ renderer.js
@@ -7,56 +7,126 @@
 var token="40f8df1703b9d48f669f84da24853fc189112da8";
 $("#mes_pwd").click(function(){
 
+
 	$.ajax({
 
 		url: "https://intranet.proxym-group.net/api/v1/passwords/",
-		dataType:"json",
-		beforeSend: function(xhr){
-        	xhr.setRequestHeaders("Authorization","Token "+token);
-        },
+		ContentType:"application/json",
+		 headers: {"Authorization": "Token "+token },
+
+        	
 		success: function(data){
-			// ici on ne veux pas que les données s'affiche dans le console mais dans la partie main de l'index
+		
 			console.log(data);
-        data.forEach(passwords =>$("#main").append(passwords.ref+"<br>"))
-    
-		}
+		
+			
+        	data.forEach(passwords =>$("#accordion").append(
+        		
+        		'<div class=card>\
+        		<div class=card-header id='+passwords.name_safe+'>\
+			      <h5 class="mb-0">\
+			        <button class="btn btn-link" data-toggle="collapse" data-target=#"'+passwords.login_safe+'" aria-expanded="false" aria-controls="'+passwords.login_safe+'">\
+			          '+passwords.name_safe+'\
+			        </button>\
+			      </h5>\
+        		</div>\
+        		<div id="'+passwords.login_safe+'" class="collapse show" aria-labelledby="'+passwords.name_safe+'" data-parent="#accordion">\
+				      <div class="card-body">\
+				        <table class="table-striped">\
+				        	<tr>\
+				        	<td>login safe</td>\
+				        	<td>'+passwords.login_safe+'</td>\
+				        	</tr>\
+				        	<tr>\
+				        	<td>password safe</td>\
+				        	<td>'+passwords.pwd_safe+'</td>\
+				        	</tr>\
+				        	<tr>\
+				        	<td>url safe</td>\
+				        	<td>'+passwords.url_safe+'</td>\
+				        	</tr>\
+				        	<tr>\
+				        	<td>note</td>\
+				        	<td>'+passwords.note+'</td>\
+				        	</tr>\
+				        	<tr>\
+				        	<td>categorie</td>\
+				        	<td>'+passwords.categorie+'</td>\
+				        	</tr>\
+				        	<tr>\
+				        	<td>owner safe</td>\
+				        	<td>'+passwords.owner_safe+'</td>\
+				        	</tr>\
+				        </table>\
+				      </div>\
+				    </div>\
+        		</div>'
+        		));
+      
+        }
+        	
 	})
-	$("#main").html(passwords[0]);
+
 })
 $("#pwd_partage").click(function(){
-
+	
 	$.ajax({
 
 		url: "https://intranet.proxym-group.net/api/v1/passwords/shared",
-		dataType:"json",
-		beforeSend: function(xhr){
-        	xhr.setRequestHeaders("Authorization","Token "+token);
-		},
+		ContentType:"application/json",
+		 headers: {"Authorization": "Token "+token },
 		success: function(data){
 			// ici on ne veux pas que les données s'affiche dans le console mais dans la partie main de l'index
 			console.log(data);
-        data.forEach(passwords =>$("#main").append(passwords.ref+"<br>"))
-    
-		}
-	})
-	$("#main").html(passwords[1]);
+        data.forEach(passwords =>$("#pwd_table").append(
+        	"<tr width=100%>"+
+        		"<td width=15%>"+passwords.name_safe+"</td>"+
+        		"<td>"+passwords.login_safe+"</td>"+
+        		"<td>"+passwords.pwd_safe+"</td>"+
+        		"<td>"+passwords.url_safe+"</td>"+
+        		"<td>"+passwords.note+"</td>"+
+        		"<td>"+passwords.categorie+"</td>"+
+        		"<td>"+passwords.owner_safe+"</td>"+
+        		"</tr>"
+        		));
+        $("#main").append("</table>");
+}
 
 })
+})
 $("#pwd_equipe").click(function(){
-
+	
 	$.ajax({
 
 		url: "https://intranet.proxym-group.net/api/v1/passwords/team",
-		dataType:"json",
-		beforeSend: function(xhr){
-        	xhr.setRequestHeaders("Authorization","Token "+token);
-		},
+		ContentType:"application/json",
+		 headers: {"Authorization": "Token "+token },
 		success: function(data){
 			// ici on ne veux pas que les données s'affiche dans le console mais dans la partie main de l'index
 			console.log(data);
-        data.forEach(passwords =>$("#main").append(passwords.ref+"<br>"))
+        data.forEach(passwords =>$("#pwd_table").append(
+        	"<tr >"+
+        		"<td>"+passwords.name_safe+"</td>"+
+        		"<td>"+passwords.login_safe+"</td>"+
+        		"<td>"+passwords.pwd_safe+"</td>"+
+        		"<td>"+passwords.url_safe+"</td>"+
+        		"<td>"+passwords.note+"</td>"+
+        		"<td>"+passwords.categorie+"</td>"+
+        		"<td>"+passwords.owner_safe+"</td>"+
+        		"</tr>"
+        		));
+        $("#main").append("</table>");
     }
-	
 	})
-	$("#main").html(passwords[2]);
+	
+})
+
+function view_list(liste){
+	document.getElementById(liste.name_safe).html=
+	"<ul id="+liste.name_safe+"><li>name safe :"+liste.name_safe+"</li><li>login safe:"+liste.login_safe+"</li><li></li><li></li><li></li><li></li><li></li></ul>";	
+}
+
+$("#search_btn").click(function(){
+
+	
 })
